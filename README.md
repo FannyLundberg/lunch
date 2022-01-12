9b3789c4a185957c063cbfdb1a19f82dbbbb55ba
Fanny Lundberg <93767753+FannyLundberg@users.noreply.github.com>
2022-01-12 10:20:10 +0100

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # lunch
+
+Skoluppgift där vi med hjälp av switch ska dagens rätt visas beroende på vilken dag det är idag.
--- main.js
+++ main.js
@@ -1,40 +1,96 @@
-let d = new Date();
-let day = d.getDay();
+// Hämta element
+const root = document.getElementById("root");
 
-console.log(day);
+// Visa "Välkommen" och bild i tre sekunder
+const welcome = document.createElement("h1");
+welcome.innerText = "Välkommen till Restaurang Semlan"
 
-    switch (day) {
-      case 1:
-        dag = "Måndag";
-        mat = "Idag serveras det pannkakor"
-        break;
-      case 2:
-        dag = "Tisdag";
-        mat = "Idag serveras det köttbullar och potatismos"
-        break;
-      case 3:
-        dag = "Onsdag";
-        mat = "Idag serveras det sushi"
-        break;
-      case 4:
-        dag = "Torsdag";
-        mat = "Idag serveras det nötfärsbiffar och potatis"
-        break;
-      case 5:
-        dag = "Fredag";
-        mat = "Idag serveras det kycklingsallad"
-        break;
-      case  6:
-        dag = "Lördag";
-        mat = "Det är stängt"
-        break;
-      case 0:
-        dag = "Söndag";
-        mat = "Det är stängt"
-        break;
-      default:
-        dag = "Okänd dag";
+const image = document.createElement("img");
+image.src = "img/semla.jpg"
 
-    }
-    document.getElementById("heading").innerHTML = dag;
-    document.getElementById("paragraf").innerHTML = mat;
+// Appenda nya element
+root.append(welcome, image)
+
+
+// Kalla på veckodagsfunktionen efter tre sekunder
+setInterval(() => {
+  
+  root.innerHTML = "";
+  
+  todaysFood();
+
+}, 3000);
+
+
+// Visa dagens mat (beroende på vilken veckodag)
+function todaysFood() {
+
+  // Variabler
+  let d = new Date();
+  let day = d.getDay();
+  
+  // Skriv ut för dagen
+  switch (day) {
+    case 1:
+      day = "Måndag";
+      food = "Idag serveras pannkakor"
+      price = "99 kronor"
+      break;
+    case 2:
+      day = "Tisdag";
+      food = "Idag serveras köttbullar och potatismos"
+      price = "99 kronor"
+      break;
+    case 3:
+      day = "Onsdag";
+      food = "Idag serveras sushi"
+      price = "99 kronor"
+      break;
+    case 4:
+      day = "Torsdag";
+      food = "Idag serveras nötfärsbiffar och potatis"
+      price = "99 kronor"
+      break;
+    case 5:
+      day = "Fredag";
+      food = "Idag serveras kycklingsallad"
+      price = "99 kronor"
+      break;
+    case 6:
+      day = "Lördag";
+      food = "Det är stängt"
+      break;
+    case 0:
+      day = "Söndag";
+      food = "Det är stängt"
+      break;
+    default:
+      day = "Okänd dag";
+
+  }
+  
+  // Skapa element för att skriva ut dag, mat och pris
+  let dayText = document.createElement("h2");
+  dayText.innerText = day;
+  root.append(dayText);
+  
+  let foodText = document.createElement("h3");
+  foodText.innerText = food;
+  root.append(foodText);
+
+  let priceText = document.createElement("h4");
+  priceText.innerText = price;
+  root.append(priceText);
+
+  // Knapp/p för att visa hela veckans meny
+  let weekMenu = document.createElement("p");
+  weekMenu.innerText = "Se hela veckans menu";
+  root.append(weekMenu);
+
+  weekMenu.addEventListener("click", showMenuWeek);
+};
+
+// Visa hela veckans meny
+function showMenuWeek() {
+  console.log("Klick på veckans meny");
+}
